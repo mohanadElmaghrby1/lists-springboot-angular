0313d163a5ff2fca9c2a7b702634f0ad5d92d79c
mohanad010050 <mohanad2021996@gmail.com>
2019-10-16 17:58:06 +0200

    Create error component

---
+++ src/main/frontend/src/app/error/error.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-error',
+  templateUrl: './error.component.html',
+  styleUrls: ['./error.component.css']
+})
+export class ErrorComponent implements OnInit {
+  errorMessage = ' this page not found back to home';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
