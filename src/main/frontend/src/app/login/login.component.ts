ffd110d6c9adf3b025f3d882c6d642346c08dc17
mohanad010050 <mohanad2021996@gmail.com>
2019-10-16 18:01:53 +0200

    Implement routing from login to welcome component

--- src/main/frontend/src/app/login/login.component.ts
+++ src/main/frontend/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -11,15 +12,19 @@ export class LoginComponent implements OnInit {
   username = 'mohannad'
   password=''
   invalidLogin = false
-  constructor() { }
+  //inject router and make it member variable
+  constructor(private router : Router) { }
 
   ngOnInit() {
   }
 
   //component event
   handleLogin() {
-    if(this.username==="mohannad" && this.password==='123')
+    if(this.username==="mohannad" && this.password==='123'){
       this.invalidLogin=false;
+      //navigate to welcome page if logged successful
+      this.router.navigate(['welcome']);
+    }
     else
       this.invalidLogin=true
     console.log(this.username)
