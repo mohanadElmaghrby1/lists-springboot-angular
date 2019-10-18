abb8f94f113335347d38b791c70da432c0e86bfd
mohannad <mohanad2021996@gmail.com>
2019-10-18 14:23:32 +0200

    Create a RouteGuardService for securing the app

--- src/main/frontend/src/app/app-routing.module.ts
+++ src/main/frontend/src/app/app-routing.module.ts
@@ -4,13 +4,17 @@ import {LoginComponent} from "./login/login.component";
 import {WelcomeComponent} from "./welcome/welcome.component";
 import {ErrorComponent} from "./error/error.component";
 import {ListTodosComponent} from "./list-todos/list-todos.component";
+import {LogoutComponent} from "./logout/logout.component";
+import {RouteGuardService} from "./service/route-guard.service";
 
 
 const routes: Routes = [
-  {path: '' , component : LoginComponent} ,
-  {path: 'login' , component : LoginComponent} ,
-  { path : 'welcome/:username' , component : WelcomeComponent} ,
-  { path : 'todos' , component : ListTodosComponent} ,
+  {path: 'login' , component : LoginComponent } ,
+  {path: 'logout' , component : LogoutComponent, canActivate:[RouteGuardService]} ,
+  {path: '' , component : LoginComponent, canActivate:[RouteGuardService]} ,
+  { path : 'welcome/:username' , component : WelcomeComponent, canActivate:[RouteGuardService]} ,
+  { path : 'todos' , component : ListTodosComponent, canActivate:[RouteGuardService]} ,
+
   { path : '**' , component : ErrorComponent} // it should be the last one
 ];
 
