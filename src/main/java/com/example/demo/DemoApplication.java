a95a003c1602db84694cb94ebd6ff02bbceb1f2f
mohanad010050 <mohanad2021996@gmail.com>
2019-10-22 13:47:07 +0200

    Add Bootstrap Todo's data

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,13 @@
 package com.example.demo;
 
+import com.example.demo.domain.Todo;
+import com.example.demo.service.TodoService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +16,12 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner runner(TodoService todoService) {
+        return args -> {
+            todoService.save(new Todo(1l,"mohannad" , "learn to code",new Date(),false));
+            todoService.save(new Todo(2l,"mohannad" , "learn to dsance",new Date(),false));
+            todoService.save(new Todo(3l,"mohannad" , "learn to sleep",new Date(),false));
+        };
+    }
 }
