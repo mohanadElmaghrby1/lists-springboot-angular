707182e8c4092d282d86b7ca621c685993668966
mohanad010050 <mohanad2021996@gmail.com>
2019-10-27 16:28:52 +0200

    Add EventEmitter for displaying todo

--- src/main/frontend/src/app/service/data/todo-data.service.ts
+++ src/main/frontend/src/app/service/data/todo-data.service.ts
@@ -1,25 +1,32 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Todo} from "../../list-todos/todo.model";
+import {TODO_URL} from "../../constant/app.constant";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoDataService {
+  onTodoSelected = new EventEmitter<Todo>()
 
 
   // onTodoRemoved = new EventEmitter<Todo>()
   constructor(private http: HttpClient) { }
 
   retrieveAllTodos(username){
-    return this.http.get(`http://localhost:8080/users/${username}/todos`);
+    return this.http.get(`${TODO_URL}users/${username}/todos`);
   }
 
   deleteTodo(id){
-    return this.http.delete(`http://localhost:8080/users/mohannad/todos/${id}`);
+    return this.http.delete(`${TODO_URL}users/mohannad/todos/${id}`);
   }
 
   getTodo(id){
-    return this.http.get(`http://localhost:8080/users/mohannad/todos/${id}`);
+    return this.http.get(`${TODO_URL}users/mohannad/todos/${id}`);
+  }
+
+  saveOrUpdateTodo(todo){
+    return this.http.post(`${TODO_URL}users/mohannad/todos/`,todo);
   }
 }
