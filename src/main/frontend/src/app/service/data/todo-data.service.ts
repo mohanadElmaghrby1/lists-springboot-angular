35476ba211f55f176af317dfb4e4a1db87b7046d
mohanadElmaghrby1 <mohanad2021996@gmail.com>
2019-12-26 17:55:06 +0200

    add token to headers

--- src/main/frontend/src/app/service/data/todo-data.service.ts
+++ src/main/frontend/src/app/service/data/todo-data.service.ts
@@ -2,6 +2,8 @@ import {EventEmitter, Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Todo} from "../../list-todos/todo.model";
 import {TODO_URL} from "../../constant/app.constant";
+import {HelloWorldBean} from "./welcome-data.service";
+import {BasicAuthenticationService} from "../basic-authentication.service";
 
 
 @Injectable({
@@ -12,33 +14,62 @@ export class TodoDataService {
 
 
   // onTodoRemoved = new EventEmitter<Todo>()
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient , private basicAuthenticationService : BasicAuthenticationService) { }
+
+
 
   retrieveAllTodos(username){
-    let basicAuthHeader = this.createBasicAuthHeader();
+    let headers = this.getHeader()
+    return this.http.get(`${TODO_URL}users/${username}/todos` ,
+      {headers});
+  }
+
+  private getHeader() {
+    let basicAuthHeader = this.basicAuthenticationService.getAuthenticatedToken();
     let headers = new HttpHeaders({
       Authorization : basicAuthHeader
     })
-
-    return this.http.get(`${TODO_URL}users/${username}/todos` , {headers});
+    return headers;
   }
 
   deleteTodo(id){
-    return this.http.delete(`${TODO_URL}users/mohannad/todos/${id}`);
+    let headers = this.getHeader()
+    return this.http.delete(`${TODO_URL}users/mohannad/todos/${id}`
+      , {headers});
   }
 
   getTodo(id){
-    return this.http.get(`${TODO_URL}users/mohannad/todos/${id}`);
+    let headers = this.getHeader()
+    return this.http.get(`${TODO_URL}users/mohannad/todos/${id}`
+  , {headers});
+
   }
+
 
   saveOrUpdateTodo(todo){
-    return this.http.post(`${TODO_URL}users/mohannad/todos/`,todo);
+    let headers = this.getHeader()
+    return this.http.post(`${TODO_URL}users/mohannad/todos/`,todo
+    ,{headers});
   }
 
-  createBasicAuthHeader():string{
-    let username = 'mohannad'
-    let password= '123'
-    let basicHeader = 'Basic '+window.btoa(username+':'+password)
-    return basicHeader;
-  }
+  // createBasicAuthHeader():string{
+  //   let username = 'mohannad'
+  //   let password= '123'
+  //   let basicHeader = 'Basic '+window.btoa(username+':'+password)
+  //   return basicHeader;
+  // }
+
+
+  // executeHelloWorldServiceWithPathVariable(name) {
+  //   let basicAuthHeaderString = this.basicAuth.getAuthenticatedToken();
+  //
+  //   let headers = new HttpHeaders({
+  //     Authorization: basicAuthHeaderString
+  //   })
+  //
+  //   return this.http.get<HelloWorldBean>(
+  //     `http://localhost:8080/hello-world/path-variable/${name}`,
+  //     {headers}
+  //   );
+  // }
 }
