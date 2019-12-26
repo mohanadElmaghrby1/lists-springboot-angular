961087d83605d98e968e4d13626d159eaefc4d02
mohanadElmaghrby1 <mohanad2021996@gmail.com>
2019-12-26 14:44:11 +0200

    Edit args

--- src/main/java/com/example/demo/repository/TodoRepository.java
+++ src/main/java/com/example/demo/repository/TodoRepository.java
@@ -4,12 +4,13 @@ import com.example.demo.domain.Todo;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * create by mohannad on 10/22/2019
  */
 public interface TodoRepository extends CrudRepository<Todo,Long> {
     List<Todo>findByUsername(String username);
-    void deleteById(long id);
-
+    void deleteByIdAndUsername(long id, String username);
+    Optional<Todo> findByIdAndUsername(long id , String username);
 }
