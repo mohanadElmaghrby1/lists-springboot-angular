2b07efdb5826651e8030434be9ad3afc7e7c7850
mohanadElmaghrby1 <mohanad2021996@gmail.com>
2019-12-26 17:55:16 +0200

    add token to headers

--- src/main/frontend/src/app/service/data/welcome-data.service.ts
+++ src/main/frontend/src/app/service/data/welcome-data.service.ts
@@ -15,11 +15,6 @@ export class WelcomeDataService {
     private http:HttpClient , private basicAuth:BasicAuthenticationService
   ) { }
 
-  executeHelloWorldBeanService() {
-    return this.http.get<HelloWorldBean>('http://localhost:8080/hello-world-bean');
-    //console.log("Execute Hello World Bean Service")
-  }
-  //http://localhost:8080/hello-world/path-variable/in28minutes
 
   executeHelloWorldServiceWithPathVariable(name) {
     let basicAuthHeaderString = this.basicAuth.getAuthenticatedToken();
@@ -32,22 +27,6 @@ export class WelcomeDataService {
       `http://localhost:8080/hello-world/path-variable/${name}`,
       {headers}
     );
-    //console.log("Execute Hello World Bean Service")
   }
 
-  // createBasicAuthenticationHttpHeader() {
-  //   let username = 'in28minutes'
-  //   let password = 'dummy'
-  //   let basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password);
-  //   return basicAuthHeaderString;
-  // }
-  //Access to XMLHttpRequest at 
-  //'http://localhost:8080/hello-world/path-variable/in28minutes' 
-  //from origin 'http://localhost:4200' has been blocked by CORS policy: 
-  //No 'Access-Control-Allow-Origin' header is present on the requested resource.
-
-  //Access to XMLHttpRequest at 'http://localhost:8080/hello-world/path-variable/in28minutes' from origin 'http://localhost:4200' 
-  //has been blocked by CORS policy: 
-  //Response to preflight request doesn't pass 
-  //access control check: It does not have HTTP ok status
 }
